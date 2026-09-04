87a3491fbe0661d760dbb48fa3c689dda1e7d18d
Typikk-lab <typikk@proton.me>
2026-09-04 14:41:56 +0300

    Create global.d.ts

---
+++ global.d.ts
@@ -0,0 +1,12 @@
+export {};
+
+declare global {
+  interface Window {
+    ethereum?: {
+      isMetaMask?: boolean;
+      request: (args: { method: string; params?: Array<any> }) => Promise<any>;
+      on?: (eventName: string, handler: (...args: any[]) => void) => void;
+      removeListener?: (eventName: string, handler: (...args: any[]) => void) => void;
+    };
+  }
+}
